550a00941ae1a9f190da8f123e7e1ddc78cc4f59
Dima <haky.gamer050@gmail.com>
2023-12-15 02:08:31 +0200

    added a validator for email-checking

--- back-end/ktor/src/main/kotlin/schedule/kpi/utils/Validator.kt
+++ back-end/ktor/src/main/kotlin/schedule/kpi/utils/Validator.kt
@@ -1,3 +1,12 @@
 package schedule.kpi.utils
 
-fun String.isValidEmail(): Boolean = false
+import java.util.regex.Pattern
+
+fun String.isValidEmail(): Boolean {
+    val emailRegex = ("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}\$")
+
+    val pattern = Pattern.compile(emailRegex)
+    val matcher = pattern.matcher(this)
+
+    return matcher.matches()
+}
