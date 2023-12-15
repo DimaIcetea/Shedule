045c4fd2e68e6cc684dfabbf1e6847921b17e8b2
Dima <haky.gamer050@gmail.com>
2023-12-15 11:47:55 +0200

    fixed bugs

--- back-end/ktor/src/main/kotlin/schedule/kpi/features/register/RegisterRouting.kt
+++ back-end/ktor/src/main/kotlin/schedule/kpi/features/register/RegisterRouting.kt
@@ -12,30 +12,10 @@ import kotlin.math.log
 fun Application.configureRegisterRouting() {
     routing {
         post("/register") {
-            val userToken = call.request.headers["Authorization"] // Получаем токен из заголовков
+            val registerController = RegisterController(call)
+            registerController.registerNewUser()
 
-            if (userToken.isNullOrEmpty()) {
-                call.respond(HttpStatusCode.Unauthorized, "Authorization token is missing")
-                return@post
-            }
 
-            val decodedToken = decodeToken(userToken)
-
-            if (decodedToken?.getClaim("admin")?.asBoolean() == true) {
-                val registerController = RegisterController(call)
-                val loginUser = decodedToken.getClaim("login")?.asString() ?: "unknown" // Предположим, что логин пользователя хранится в токене
-                registerController.registerNewUser()
-                val message = RegisterResponseModel(login = loginUser, message = "admin registered")
-                call.respond(HttpStatusCode.Created, message)
-            }
-            else{
-                val registerController = RegisterController(call)
-                val loginUser = decodedToken?.getClaim("login")?.asString() ?: "unknown" // Предположим, что логин пользователя хранится в токене
-                registerController.registerNewUser()
-                val message = RegisterResponseModel(login = loginUser, message = "user registered")
-                call.respond(HttpStatusCode.Created, message)
-            }
         }
-
     }
 }
